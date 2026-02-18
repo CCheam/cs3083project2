fe60095104b0281881494d09fb78d73fce484118
CCheam <ccheam25@gmail.com>
2026-02-18 12:48:40 -0600

    maybe finished game victory checks

--- connect4.py
+++ connect4.py
@@ -34,13 +34,13 @@ def game_evaluation(board,player):
             if all(board[r+i,c] == player for i in range(4)):
                 return True
     #diagonal checks
-    for r in range(rows):
+    for r in range(3,rows):
         for c in range(cols-3):
-            if all(board[r,c+i] == player for i in range(4)):
+            if all(board[r-i,c+i] == player for i in range(4)):
                 return True
     for r in range(rows-3):
-        for c in range(cols):
-            if all(board[r+i,c] == player for i in range(4)):
+        for c in range(cols-3):
+            if all(board[r+i,c+i] == player for i in range(4)):
                 return True
     return False
 
