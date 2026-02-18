e4a791bac34a89cfdbc9233dafe2f9846a835d67
CCheam <ccheam25@gmail.com>
2026-02-18 11:09:25 -0600

    finish basic setup for part 1

--- connect4.py
+++ connect4.py
@@ -1,4 +1,18 @@
-import numpy
+import numpy as np
 import matplotlib.pyplot as mtpy
 import time
 import os
+
+def game(board):
+    return -1
+def main():
+    board =np.arr(['*','*','*','*','*','*','*'],
+                  ['*','*','*','*','*','*','*'],
+                  ['*','*','*','*','*','*','*'],
+                  ['*','*','*','*','*','*','*'],
+                  ['*','*','*','*','*','*','*'],
+                  ['*','*','*','*','*','*','*'])
+    print(board)
+
+if __name__ == "__main__":
+    main()
