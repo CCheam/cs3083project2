4f1001cab16aee59bcaebbb59ca86b2aa08860ef
CCheam <ccheam25@gmail.com>
2026-02-18 11:15:55 -0600

    stage pseudocode for gameplay

--- connect4.py
+++ connect4.py
@@ -3,7 +3,18 @@ import matplotlib.pyplot as mtpy
 import time
 import os
 
+def opponent_ai(board):
+    return
+def game_evaluation(board):
+    win = ''
+    if ():
+        return True
+    return False
+
 def game(board):
+    while not game_evalution(board):
+        #play game
+        return 
     return -1
 def main():
     board =np.arr(['*','*','*','*','*','*','*'],
