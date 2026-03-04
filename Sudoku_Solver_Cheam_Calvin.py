cf4822cab4f04a938776339c7aac042246f3e988
CCheam <ccheam25@gmail.com>
2026-03-04 12:21:40 -0600

    Function works, have to tweak it so that it prints properly

---
+++ Sudoku_Solver_Cheam_Calvin.py
@@ -0,0 +1,75 @@
+import numpy as np
+import matplotlib.pyplot as mtpy
+import json
+import time
+import os
+
+#
+#Drawing heavily from https://www.geeksforgeeks.org/dsa/sudoku-backtracking-7/ for unoptimized backtracking, but tweaked to scale w 
+#block size. Optimization strategies are my own code. 
+#
+
+def valid_sq_check(board,row,col,num,block_size):
+
+    #check by row, set to be col for scalability
+    for x in range(col):
+        if(board[row][x] == num):
+            return False
+    #check by row, set to be col for scalability
+    for x in range(row):
+        if(board[x][col] == num):
+            return False
+        
+    #loop through square
+    sRow = row - (row % block_size)
+    sCol = col - (col % block_size)
+
+    for i in range(block_size):
+        for j in range(block_size):
+            if(board[sRow+i][sCol+j] == num):
+                return False
+    return True
+        
+def sudoku_solved_uo(board,row,col,b_size):
+    b_end = b_size * b_size
+    #end looping at end of rows
+    if row == (b_end -1) and col == (b_end):
+        return True
+    #col looping
+    if col == b_end:
+        row+=1
+        col=0
+    #pass over occupied sqs
+    if board[row][col] != 0:
+        return(sudoku_solved_uo(board,row,col+1,b_size))
+
+    for num in range(1,b_end+1):
+        if (valid_sq_check(board,row,col,num,b_size)):
+            board[row][col]=num
+            if sudoku_solved_uo(board,row,col+1,b_size):
+                return True
+            board[row][col]=0
+            
+    return False
+    
+def solve_sudoku(board,size):
+    sTime=time.perf_counter()
+    sudoku_solved_uo(board,0,0,size)
+    eTime=time.perf_counter()
+    rTime=eTime-sTime
+    print(f"Time to run: {rTime:.6f} seconds")
+
+
+def main():
+    block_size = 4
+    empty_sqs = 30
+    block_size_sq = block_size * block_size
+    block_file= f"sudoku{block_size_sq}.json"
+    #dynamically run sudoku generator based off of passed in vars
+    os.system(f"python SudokuGenerator.py {empty_sqs} --output_file {block_file} --block_size {block_size}")
+    with open(block_file, "r") as f:
+        board = json.load(f)
+    solve_sudoku(board,block_size)
+
+if __name__ == "__main__":
+    main()
